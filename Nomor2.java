4ac4d117c3cb15e90d34f6e433464d39db17a622
JohanesDarren <johanesdarrenyehuda@student.telkomuniversity.ac.id>
2024-10-24 08:41:30 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Nomor2{
     public static void main(String[] args){
+        int totalOmzet = 0;
+        int biayaTrip = 13000;
+        Scanner input = new Scanner(System.in);
 
+         try {
+                System.out.println("Masukkan Nama Driver (Beliau/Mereka/Kita) :"); 
+                String nama = input.nextLine();
+
+                System.out.print("Masukkan lama trip: ");
+                int lamaTrip = input.nextInt();
+
+                System.out.println("Input lagi? (Y/N)");
+                String lagi = input.nextLine();
+                String lanjut = "";
+                switch (lagi) {
+                    case 1:
+                        lagi = "Y";
+                        break;
+                    case 2:
+                        lagi = "N";
+                        break;
+                    default:
+                      System.out.println("Driver: " + nama);
+                      System.out.println("lama trip: " + lamaTrip);
+                      System.out.println("total trip" + (lamaTrip));
+                return;
+
+                System.out.println("Total trip semua driver: " + ( lamaTrip ));
+                System.out.println("Omzet tertinggi diperoleh " + nama + " dengan total omzet " + (lamaTrip *biayaTrip));
+
+            } 
+        }
     }
 }
