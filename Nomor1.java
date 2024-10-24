484c91048e779ee238f3fb223da4936ecf5eff52
JohanesDarren <johanesdarrenyehuda@student.telkomuniversity.ac.id>
2024-10-24 08:37:09 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class Nomor1{
     public static void main(String[] args){
+        int totalHonor = 0;
+        int jamLembur;
+        double honorPerJam;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan jumlah jam lembur anda:");
+        jamLembur = input.nextInt();
+
+        for (int i = 1; i <= jamLembur; i++) {
+            if (i > 16 ) {
+                if (jamLembur <= 16) {
+                    jamLembur = jamLembur * 10000;
+                }
+                        else {
+                            jamLembur = jamLembur * 15000;
+                        }
+                    }
+                    totalHonor += jamLembur;
+                }
+        System.out.println("Total jam lembur anda: " + jamLembur);
+        System.out.println("Total Honor lembur anda: " + (totalHonor += jamLembur));
 
     }
 }
